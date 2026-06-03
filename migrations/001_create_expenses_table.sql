fa6de020ee46989bed146c3fa0861674bdd35f21
Mohammadreza <mrsafaran1380@gmail.com>
2026-06-03 10:58:56 -0700

    Add goose database migrations

--- migrations/001_create_expenses_table.sql
+++ migrations/001_create_expenses_table.sql
@@ -1,6 +1,12 @@
+-- +goose Up
+
 CREATE TABLE expenses (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     amount NUMERIC NOT NULL,
     category TEXT NOT NULL
 );
+
+-- +goose Down
+
+DROP TABLE expenses;
